d283b3610c41951ae857786fed45008223dbb7d0
VaSeBa <badasin.valentin@yandex.ru>
2020-12-16 21:58:49 +0300

    Initial commit

---
+++ src/main/java/ru/vaseba/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package ru.vaseba.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String seyHello(){
+        return "hello_world";
+    }
+}
